c15150a7ff8bc7b0ce92f090f441924cd978c178
Joshua Anderton <info@joshanderton.com>
2022-03-08 21:30:19 -0800

    Generated class files for all css properties

--- src/View/Attributes/ClassName.php
+++ src/View/Attributes/ClassName.php
@@ -2,8 +2,6 @@
 
 namespace TallSaas\View\Attributes;
 
-use TallSaas\View\Attributes\Style\Properties;
-
 use Illuminate\Support\Collection;
 
 class ClassName extends Collection 
@@ -11,25 +9,5 @@ class ClassName extends Collection
   public function string()
   {
     return $this->implode(' ');
-  }
-
-  public function createCssClassFiles(): void
-  {
-    $properties = Properties::cases();
-
-    foreach($properties as $property) :
-      $this->createCssClassFile($property->name);
-    endforeach;
-  }
-
-  public function createCssClassFile(string $className)
-  {
-    $className = ucfirst($property->name);
-
-    $classFile = fopen("Style/{$className}.php", "w");
-
-    $txt = "<?php\n\nnamespace TallSaas\View\Attributes;\n\nclass {$className}\n{\n\n  //\n\n}";
-
-    fwrite($classFile, $txt);
   }
 }
---
+++ src/View/Attributes/Style/Orphans.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TallSaas\View\Attributes\Style;
+
+class Orphans
+{
+  //
+}
